6dd41d1c5ee9a8f83d9699ce1d820a4965fc50c1
conner.ferguson@dmescripts.com <conner.ferguson@dmescripts.com>
2023-04-11 23:04:48 +0000

    deleted files and moved stuff around

--- models/legacy/customer_orders.sql
+++ models/legacy/customer_orders.sql
@@ -1,144 +1,63 @@
-WITH
+with
+    paid_orders as (
+        select
+            orders.id as order_id,
+            orders.user_id as customer_id,
+            orders.order_date as order_placed_at,
+            orders.status as order_status,
+            p.total_amount_paid,
+            p.payment_finalized_date,
+            c.first_name as customer_first_name,
+            c.last_name as customer_last_name
+        from raw.jaffle_shop.orders as orders
+        left join
+            (
+                select
+                    orderid as order_id,
+                    max(created) as payment_finalized_date,
+                    sum(amount) / 100.0 as total_amount_paid
+                from raw.stripe.payment
+                where status <> 'fail'
+                group by 1
+            ) p
+            on orders.id = p.order_id
+        left join raw.jaffle_shop.customers c on orders.user_id = c.id
+    ),
 
--- Import CTEs
-customers as (
+    customer_orders as (
+        select
+            c.id as customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(orders.id) as number_of_orders
+        from raw.jaffle_shop.customers c
+        left join raw.jaffle_shop.orders as orders on orders.user_id = c.id
+        group by 1
+    )
 
-    select * from {{ source('jaffle_shop', 'customers') }}
-
-),
-
-base_orders as (
-
-    select * from {{ source('jaffle_shop', 'orders') }}
-
-),
-
-payments as (
-
-    select * from {{ source('stripe', 'payment') }}
-
-),
-
--- Logical CTEs
-customers as (
-
-    select 
-
-        first_name || ' ' || last_name as name, 
-        * 
-
-    from customers
-
-),
-
-orders as (
-
-      select 
-
-        row_number() over (
-            partition by user_id 
-            order by order_date, id
-        ) as user_order_seq,
-        *
-
-      from orders
-
-),
-
--- marts
-
-customer_order_history as (
-
-    select 
-
-        customers.customer_id,
-        customers.full_name,
-        customers.surname,
-        customers.givenname,
-
-        min(order_date) as first_order_date,
-
-        min(case 
-            when a.status not in ('returned','return_pending') 
-            then order_date 
-        end) as first_non_returned_order_date,
-
-        max(case 
-            when a.status not in ('returned','return_pending') 
-            then order_date 
-        end) as most_recent_non_returned_order_date,
-
-        coalesce(max(user_order_seq),0) as order_count,
-
-        coalesce(count(case 
-            when a.status != 'returned' 
-            then 1 end),
-            0
-        ) as non_returned_order_count,
-
-        sum(case 
-            when a.status not in ('returned','return_pending') 
-            then round(c.amount/100.0,2) 
-            else 0 
-        end) as total_lifetime_value,
-
-        sum(case 
-            when a.status not in ('returned','return_pending') 
-            then round(c.amount/100.0,2) 
-            else 0 
-        end)
-        / nullif(count(case 
-            when a.status not in ('returned','return_pending') 
-            then 1 end),
-            0
-        ) as avg_non_returned_order_value,
-
-        array_agg(distinct a.id) as order_ids
-
-    from a
-
-    join customers
-    on a.user_id = customers.id
-
-    left outer join payments as c
-    on a.id = c.orderid
-
-    where a.status not in ('pending') and c.status != 'fail'
-
-    group by customers.id, customers.name, customers.last_name, customers.first_name
-
-),
-
--- Final CTEs 
-final as (
-
-    select 
-
-        orders.id as order_id,
-        orders.user_id as customer_id,
-        last_name as surname,
-        first_name as givenname,
-        first_order_date,
-        order_count,
-        total_lifetime_value,
-        round(amount/100.0,2) as order_value_dollars,
-        orders.status as order_status,
-        payments.status as payment_status
-
-    from orders
-
-    join customers
-    on orders.user_id = customers.id
-
-    join customer_order_history
-    on orders.user_id = customer_order_history.customer_id
-
-    left outer join payments
-    on orders.id = payments.orderid
-
-    where payments.status != 'fail'
-
-)
-
--- Simple Select Statement
-SELECT * FROM FINAL
+select
+    p.*,
+    row_number() over (order by p.order_id) as transaction_seq,
+    row_number() over (
+        partition by customer_id order by p.order_id
+    ) as customer_sales_seq,
+    case
+        when c.first_order_date = p.order_placed_at then 'new' else 'return'
+    end as nvsr,
+    x.clv_bad as customer_lifetime_value,
+    c.first_order_date as fdos
+from paid_orders p
+left join customer_orders as c using (customer_id)
+left outer join
+    (
+        select p.order_id, sum(t2.total_amount_paid) as clv_bad
+        from paid_orders p
+        left join
+            paid_orders t2
+            on p.customer_id = t2.customer_id
+            and p.order_id >= t2.order_id
+        group by 1
+        order by p.order_id
+    ) x
+    on x.order_id = p.order_id
+order by order_id
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,24 +1,27 @@
 
 
-WITH SOURCE AS (
+WITH 
 
-SELECT * FROM {{ source('stripe', 'payment') }}
+SOURCE AS (
+
+        SELECT * FROM {{ source('stripe', 'payment') }}
 
 ),
 
 TRANSFORMED AS (
 
-SELECT
-    id              as payment_id,
-    orderid         as order_id,
-    paymentmethod   as payment_method,
-    status          as payment_status,
+        SELECT
 
-    {{ cents_to_dollars('amount') }}    as payment_amount,
-    created         as created_at
-
-FROM
-        SOURCE
+            id              as payment_id,
+            orderid         as order_id,
+            paymentmethod   as payment_method,
+            status          as payment_status,
+        
+            {{ cents_to_dollars('amount') }}    as payment_amount,
+            created         as created_at
+        
+        FROM
+                SOURCE
 
 )
 
