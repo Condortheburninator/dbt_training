d9318ad2e2490aaacb792cea408d506372f6503f
conner.ferguson@dmescripts.com <conner.ferguson@dmescripts.com>
2023-04-11 17:54:44 +0000

    updates

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,7 +1,10 @@
-with
-    customers as (select * from {{ ref("stg_customers") }}),
-    orders as (select * from {{ ref("fct_orders") }}),
-    customer_orders as (
+
+
+WITH
+     customers      AS ( SELECT * FROM {{ ref("stg_customers") }})
+    ,orders         AS ( SELECT * FROM {{ ref("fct_orders") }})
+    ,employees      AS ( SELECT * FROM {{ ref('employees' ) }} )
+    ,customer_orders as (
         select
             customer_id,
             min(order_date) as first_order_date,
@@ -10,18 +13,25 @@ with
             sum(amount) as lifetime_value
         from orders
         group by 1
-    ),
-    final as (
-        select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-            customer_orders.lifetime_value
-        from customers
-        left join customer_orders using (customer_id)
     )
-select *
-from final
+    ,
+    FINAL as (
+        
+        SELECT
+                customers.customer_id,
+                customers.first_name,
+                customers.last_name,
+                employees.employee_id IS NOT NULL AS is_employee,
+                customer_orders.first_order_date,
+                customer_orders.most_recent_order_date,
+                coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+                customer_orders.lifetime_value
+        
+        FROM
+                customers
+        
+        LEFT JOIN customer_orders USING (customer_id)
+        LEFT JOIN employees USING (customer_id)
+    )
+
+SELECT * FROM FINAL
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,7 +4,7 @@ SELECT
         id          as order_id,
         user_id     as customer_id,
         order_date,
-        status
+        status      as order_status
 
 FROM
         {{ source('jaffle_shop', 'orders') }}
