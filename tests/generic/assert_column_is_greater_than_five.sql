acffa99a86f0ad31688c005e2ae29ebdc332e0b8
conner.ferguson@dmescripts.com <conner.ferguson@dmescripts.com>
2023-04-12 15:50:45 +0000

    initial commit & added test to source yml file

---
+++ tests/generic/assert_column_is_greater_than_five.sql
@@ -0,0 +1,14 @@
+
+
+{% test assert_column_is_greater_than_five(model, column_name) %}
+
+SELECT
+        {{ column_name }}
+
+FROM
+        {{ model }}
+
+WHERE
+        {{ column_name }} <= 5
+
+{% endtest %}
